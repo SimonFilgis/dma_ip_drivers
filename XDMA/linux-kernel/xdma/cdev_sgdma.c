bb1eb1fd6ecccb5fcb3b34694ad5f6d3d9e2d192
Simon Filgis <simon@ingenieurbuero-filgis.de>
2026-08-05 10:43:10 +0200

    reduce std timeout

--- XDMA/linux-kernel/xdma/cdev_sgdma.c
+++ XDMA/linux-kernel/xdma/cdev_sgdma.c
@@ -36,13 +36,13 @@
 #include "xdma_thread.h"
 
 /* Module Parameters */
-unsigned int h2c_timeout = 10;
+unsigned int h2c_timeout = 1;
 module_param(h2c_timeout, uint, 0644);
-MODULE_PARM_DESC(h2c_timeout, "H2C sgdma timeout in seconds, default is 10 sec.");
+MODULE_PARM_DESC(h2c_timeout, "H2C sgdma timeout in seconds, default is 1 sec.");
 
-unsigned int c2h_timeout = 10;
+unsigned int c2h_timeout = 1;
 module_param(c2h_timeout, uint, 0644);
-MODULE_PARM_DESC(c2h_timeout, "C2H sgdma timeout in seconds, default is 10 sec.");
+MODULE_PARM_DESC(c2h_timeout, "C2H sgdma timeout in seconds, default is 1 sec.");
 
 extern struct kmem_cache *cdev_cache;
 static void char_sgdma_unmap_user_buf(struct xdma_io_cb *cb, bool write);
